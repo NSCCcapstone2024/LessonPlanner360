c741c15db763f71dba65d2e71ec40dc2e1651d93
nomanymore <nomanymore5@gmail.com>
2024-01-17 20:13:21 -0400

    Initial commit

---
+++ mysql/tbllogin.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: May 04, 2017 at 03:29 AM
+-- Server version: 5.6.35
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dotnetcoreSamples`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblLogin`
+--
+
+CREATE TABLE `tblLogin` (
+  `username` varchar(45) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `salt` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblLogin`
+--
+-- password = test
+INSERT INTO `tblLogin` (`username`, `password`, `salt`) VALUES
+('user', 'uLzOc9hqo47A75r1r9TE3ZctD3qmWEA4oQip4zfpgMg=', 'KUgMBBIZbPDsMiGUOc1UvQ=='),
+('victoria', 'BGiogj+634vjrzZskE8UkQLpOMtQtbNY4yRpDoKkZBQ=','5oyQOm8lAsCo4iqjtyYZcQ==');
+
+
+--
+-- Indexes for table `tblLogin`
+--
+ALTER TABLE `tblLogin`
+  ADD PRIMARY KEY (`username`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
