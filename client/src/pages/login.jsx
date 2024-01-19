c94e447f9867ee88e1f94984ab6470e268d3200b
Harmandeep Kaur <hrmndp2003@gmailcom>
2024-01-19 13:00:07 -0400

    delete some useless code

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,21 +1,3 @@
-
-// export default function Login() {
-
-//     return (
-//         <div>
-//             <form>
-//                 <label htmlFor="username">Username:</label>
-//                 <input type="text" id="username" name="username" required />
-
-//                 <label htmlFor="password">Password:</label>
-//                 <input type="password" id="password" name="password" required />
-
-//                 <button type="submit">Login</button>
-//             </form>
-//         </div>
-//     );
-// }
-
 import React from 'react';
 
 export default function Login() {
