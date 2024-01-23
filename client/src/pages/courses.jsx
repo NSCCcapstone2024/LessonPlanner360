82e5042b5c3cf7773e8a4cb24669cd18bc9a82ac
nomanymore <nomanymore5@gmail.com>
2024-01-22 20:39:35 -0400

    Created a "courses" table in the database

--- client/src/pages/courses.jsx
+++ client/src/pages/courses.jsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 
-export default function Login() {
+export default function Courses() {
 
     return (
         <p>This is the courses page!</p>
---
+++ mysql/tblcourses.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: May 04, 2017 at 03:29 AM
+-- Server version: 5.6.35
+-- PHP Version: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcourses`
+--
+
+CREATE TABLE `tblCourses` (
+  `id` int(8) NOT NULL AUTO_INCREMENT,
+  `course_code` varchar(200) NOT NULL,
+  `course_name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblCourses`
+--
+-- 
+INSERT INTO `tblCourses` (`id`, `course_code`, `course_name`) VALUES
+('1', 'WEBD3027', 'Developing for CMS'),
+('2', 'WEBD3000', 'Web Application Programming II'),
+('3', 'INFT3000', 'Capstone'),
+('4', 'INET2005', 'Web Application Programming I'),
+('5', 'INFT2100','Project Management');
+
+
+--
+-- Indexes for table `tblCourses`
+--
+ALTER TABLE `tblCourses`
+  ADD PRIMARY KEY (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
