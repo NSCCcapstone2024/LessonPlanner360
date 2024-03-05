1209455406c5c8cc558b978484327f688c99ace1
nomanymore <nomanymore5@gmail.com>
2024-03-04 22:44:58 -0400

    added simple form to add lessons, no functionality

--- client/src/pages/[courseId].jsx
+++ client/src/pages/[courseId].jsx
@@ -11,6 +11,54 @@ export default function Lessons() {
     const [loading, setLoading] = useState(true);
     const { data: session } = useSession();
 
+    // POPUPS
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+    // Error message
+    const [errorMessage, setErrorMessage] = useState('');
+
+    // New lesson states
+    const [newLesson, setNewLesson] = useState({
+        unit_number: '',
+        week: '',
+        class_ID: '',
+        learning_outcomes: '',
+        enabling_outcomes: '',
+        material: '',
+        assessment: '',
+        notes: '',
+    });
+
+    // ------------------ ADDING LESSONS--------------------
+
+
+    const handleAddLesson = () => {
+        setIsPopupOpen(true);
+        setErrorMessage('');
+    };
+
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewLesson((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+
+
+    // ----------------FILE UPLOADS-------------------
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            setNewLesson((prevState) => ({
+                ...prevState,
+                material: file,
+            }));
+        }
+    };
+
+    //  ----------------------- FETCHING FUNCTIONS ------------------
     useEffect(() => {
         if (!courseId) return;
         const fetchLessons = async () => {
@@ -31,6 +79,7 @@ export default function Lessons() {
         fetchLessons();
     }, [courseId]);
 
+    // toggle the accordion
     const toggleLessonOpen = (lessonId) => {
         const updatedLessons = lessons.map((lesson) =>
             lesson.id === lessonId ? { ...lesson, isOpen: !lesson.isOpen } : lesson
@@ -41,17 +90,21 @@ export default function Lessons() {
     useEffect(() => {
         // Ensure we only fetch data if the user is authenticated
         if (!session) {
-            router.push('/login'); // Redirect to login page if not authenticated
+            router.push('/login');
         }
     }, [session, router]);
 
-    // Handle logout
+
+    // ------------------ LOGOUT --------------------
     const handleSignOut = () => {
-        signOut({ callbackUrl: '/login' }); // Sign out and redirect to login page
+        signOut({ callbackUrl: '/login' });
     };
 
-    const handleAddLesson = () => {
-        // Add navigation logic to the add lesson page
+    // close and reset all fields when cancel is clicked
+    const closePopupAndResetForm = () => {
+        setIsPopupOpen(false);
+        setNewLesson({ unit_number: '', week: '', class_ID: '', learning_outcomes: '', enabling_outcomes: '', material: '', assessment: '', notes: '' });
+        setErrorMessage('');
     };
 
     return (
@@ -80,8 +133,62 @@ export default function Lessons() {
                             ))}
                         </div>
                     )}
+
                 </div>
             )}
+            {isPopupOpen && (
+                <div className="fixed top-0 left-0 w-full h-full bg-gray-900 bg-opacity-50 flex justify-center items-center">
+                    <div className="bg-gray-300 p-20 rounded-lg overflow-y-auto max-h-[90vh]">
+                        <h2 className="text-xl font-bold mb-4">Add New Lesson</h2>
+                        <div className="mb-6">
+                            <label htmlFor="unit_number" className="block text-sm font-medium text-gray-700 mb-1">Unit Number</label>
+                            <select id="unit_number" name="unit_number" value={newLesson.unit_number} onChange={handleInputChange} className="border-gray-300 border rounded-md p-2 block w-1/4">
+                                {[...Array(10)].map((_, i) => (
+                                    <option key={i} value={i + 1}>{i + 1}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="week" className="block text-sm font-medium text-gray-700 mb-1">Week</label>
+                            <select id="week" name="week" value={newLesson.week} onChange={handleInputChange} className="border-gray-300 border rounded-md p-2 block w-1/4">
+                                {[...Array(52)].map((_, i) => (
+                                    <option key={i} value={i + 1}>{i + 1}</option>
+                                ))}
+                            </select>
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="class_ID" className="block text-sm font-medium text-gray-700 mb-1">class ID</label>
+                            <input type="text" id="class_ID" name="class_ID" value={newLesson.class_ID} onChange={handleInputChange} placeholder="Enter class ID" className="border-gray-300 border rounded-md p-2 block w-96" maxLength={200} />
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="learning_outcomes" className="block text-sm font-medium text-gray-700 mb-1">Learning Outcomes</label>
+                            <input type="text" id="learning_outcomes" name="learning_outcomes" value={newLesson.learning_outcomes} onChange={handleInputChange} placeholder="Enter Learning Outcomes" className="border-gray-300 border rounded-md p-2 block w-96" maxLength={200} />
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="enabling_outcomes" className="block text-sm font-medium text-gray-700 mb-1">Enabling Outcomes</label>
+                            <input type="text" id="enabling_outcomes" name="enabling_outcomes" value={newLesson.enabling_outcomes} onChange={handleInputChange} placeholder="Enter Enabling Outcomes" className="border-gray-300 border rounded-md p-2 block w-96" maxLength={200} />
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="material" className="block text-sm font-medium text-gray-700 mb-1">Material</label>
+                            <input type="file" id="material" name="material" onChange={handleFileChange} className="border-gray-300 border rounded-md p-2 block w-full" />
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="assessment" className="block text-sm font-medium text-gray-700 mb-1">Assessment</label>
+                            <input type="text" id="assessment" name="assessment" value={newLesson.assessment} onChange={handleInputChange} placeholder="Enter Assessment" className="border-gray-300 border rounded-md p-2 block w-96" maxLength={200} />
+                        </div>
+                        <div className="mb-6">
+                            <label htmlFor="notes" className="block text-sm font-medium text-gray-700 mb-1">Notes</label>
+                            <input type="text" id="notes" name="notes" value={newLesson.notes} onChange={handleInputChange} placeholder="Enter Notes" className="border-gray-300 border rounded-md p-2 block w-96" maxLength={200} />
+                        </div>
+                        <div className="flex justify-between">
+                            <button onClick={handleAddLesson} className="bg-blue-500 text-white px-4 py-2 rounded-md mr-2">Add Lesson</button>
+                            <button onClick={closePopupAndResetForm} className="bg-gray-500 text-white px-4 py-2 rounded-md">Cancel</button>
+                        </div>
+                        {errorMessage && <div className="text-red-500 mt-2">{errorMessage}</div>}
+                    </div>
+                </div>
+            )
+            }
         </div>
     );
 }
